8b0b632d094ffc45d0caba50ee275a72eddd14f3
LiwaBD <37658985+LiwaBD@users.noreply.github.com>
2022-12-12 23:38:53 +0100

    Update TestParallelAgent.java

--- Projet_SMA/src/Projet_SMA/TestParallelAgent.java
+++ Projet_SMA/src/Projet_SMA/TestParallelAgent.java
@@ -1,8 +1,6 @@
 package Projet_SMA;
 
 import jade.core.Agent;
-import jade.core.AID;
-import jade.domain.AMSService;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.*;
 import jade.domain.FIPAException;
@@ -34,43 +32,27 @@ public class TestParallelAgent extends Agent
 			DFAgentDescription[] result = DFService.search(this, dfd);
 			
 			System.out.println("Search returns: " + result.length + " compute agents" );
-//			System.out.println(getService("Compute Agent"));
+		    double[] partial_integral = new double[result.length];
+		    double[] new_min = new double[result.length];
+		    double[] new_max = new double[result.length];
+		    double sum = 0;
 			for (int i=0; i<result.length;i++)
 			{
-			    double[] partial_integral = new double[result.length];
-			    double[] new_min = new double[result.length];
-			    double[] new_max = new double[result.length];
-			    
-			    new_min[i]=min;
-			    new_max[i]=max/result.length;
-			    new_min[i]=new_max[i];
-			    new_max[i] += new_min[i];
-			    
-	//			String ca_name[] = (String) result[i].toString();
 
-//			    for(double j = min + delta; j < i; j += delta)
-//			    {
-			    partial_integral[i] = Function.MyFunction(min,max,delta);
-				System.out.println("ComputeAgent " + i + " replies: " + partial_integral[i]);
-//				}
+			    double devider = (max - min)/result.length;
+			    new_min[i] = min + (i*devider);
+			    new_max[i] = min + ((i+1)*devider);
+			      
+			    partial_integral[i] = Function.MyFunction(new_min[i],new_max[i],delta);
+			    sum+= partial_integral[i];
+				System.out.println("ComputeAgent " + result[i].getName().getLocalName()+ " replies: " + partial_integral[i]);
 			}
-				
+			System.out.println("sum = "+ sum);	
 			 long startTime = System.currentTimeMillis();
 		     Single_Integral = Projet_SMA.Function.MyFunction(min,max,delta);
 		     long endTime = System.currentTimeMillis();
 			System.out.println("** Single result = " + Single_Integral + "  Done in :" + (endTime-startTime) + " ms");
 
-//			if (result.length>0)
-//				System.out.println(" " + result[0].getName() );
-
-			
-//			sd  = new ServiceDescription();
-//			sd.setType( "buyer" );
-//			dfd.addServices(sd);
-//			result = DFService.search(this, dfd);
-//			System.out.println("Search for BUYER: " + result.length + " elements" );
-//			if (result.length>0)
-//				System.out.println(" " + result[0].getName() );
         }
         catch (FIPAException fe) { fe.printStackTrace(); }
     }
